a43afdbb52be45be840c4fa966f36fb5177a77f8
plumseed <leawq@outlook.com>
2025-05-26 00:59:36 +0800

    配置Webpack和Babel
    在项目根目录添加 webpack.renderer.config.js，用于打包渲染进程代码

---
+++ webpack.renderer.config.js
@@ -0,0 +1,36 @@
+// webpack.renderer.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/renderer.js',
+  output: {
+    path: path.resolve(__dirname, 'out'),
+    filename: 'renderer.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    hot: true,
+    port: 3000,
+  },
+};
