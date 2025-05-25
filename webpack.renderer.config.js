748159b22f8b81fe93924998b238ed7b6dce7c33
plumseed <leawq@outlook.com>
2025-05-26 02:45:30 +0800

    fix error
    你这次报的错误来自于 Babel 没有启用 JSX 的转换（@babel/preset-react），以及你的 Webpack config 没有指定 mode。我们只需要做两件事：
    1 安装并启用 @babel/preset-react
    1.1 安装 preset
    1.2 添加 Babel 配置
    2 在 Webpack 配置里指定 mode

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'production',
   entry: './src/renderer.js',
   output: {
     path: path.resolve(__dirname, 'out'),
